220c73f1639723489b5c731d5a8f48c8c1f24d27
Ilya Fadeev <ilya@Ilya-Fadeevs-MacBook-Pro.local>
2010-10-15 10:54:55 -0400

    added params for Layouts.

--- layouts.js
+++ layouts.js
@@ -1,13 +1,23 @@
-var cell_size = 4;
-var cell_margin = 1;
 
-var style_init = function(params){
+var Layouts = function(params){
 
-	/*var params = params || {};
-	var cell_size 	= params.cell_size || 4;
-	var cell_margin = params.cell_margin || 1;*/
+	var params = params || {};
+	
+	// actual size of a cell in px:
+	this.cell_size 	= params.cell_size || 4;
+	this.cell_margin = params.cell_margin || 1;
+	this.grid_max	= params.grid_max || 16;
+	
+	this.prefix = 'grid_' + this.cell_size + '__';
+	
+	this.style_init();
+}
+Layouts.prototype.style_init = function(){
 
-	var grid_max = 16;
+	var cell_size 	= this.cell_size;
+	var cell_margin = this.cell_margin;
+	var grid_max 	= this.grid_max;
+	
 	var styles = '';
 	
 	// margin correction:
@@ -16,101 +26,93 @@ var style_init = function(params){
 	for (var i = 1; i <= grid_max; i++){
 		
 		// square:
-		styles += '.grid_' + i + '{ height: ' + (i*cell_size - m_corr) + 'px; width: ' + (i*cell_size - m_corr) + 'px; }\n';
+		styles += '.' + this.prefix + i + '{ height: ' + (i*cell_size - m_corr) + 'px; width: ' + (i*cell_size - m_corr) + 'px; }\n';
 		
 		// rectangle:
 		for (var j = 1; j <= grid_max; j++){
-			styles += '.grid_' + i + '_' + j + '{ height: ' + i*cell_size + 'px; width: ' + j*cell_size + 'px; }\n';
+			styles += '.' + this.prefix + i + '_' + j + '{ height: ' + i*cell_size + 'px; width: ' + j*cell_size + 'px; }\n';
 		}
 	}
 	
 	styles += '.grid_container{position: relative;}';
-	styles += '.grid_cell{ display: inline; float: left; position: relative; margin: ' + cell_margin + 'px; background: #cdc;}';
+	styles += '.' + this.prefix + 'cell{ display: inline; float: left; position: relative; margin: ' + cell_margin + 'px; background: #cdc;}';
 	
 	//alert(styles);
-	jQuery('body').prepend('<style>' + styles + '</style>');
-}();
-
-var Layouts = function(type){
-
-	// actual size of a cell in px:
-	var cell_size = 10;
+	jQ('body').prepend('<style>' + styles + '</style>');
 	
-	var layouts = [
-		{	size: 6,
-			cells:[
-				[1,1],
-				[1,1]]},
-		{	size: 4,
-			cells:[
-				[2,0,1],
-				[0,0,1],
-				[1,1,1]]},
-		{	size: 4,
-			cells:[
-				[1,1,1],
-				[1,1,1],
-				[1,1,1]]},
-		{	size: 4,
-			cells:[
-				[1,1,1,1],
-				[2,0,2,0],
-				[0,0,0,0]]},
-		{	size: 3,
-			cells:[
-				[2,0,2,0],
-				[0,0,0,0],
-				[2,0,1,1],
-				[0,0,1,1]]},
-		{	size: 3,
-			cells:[
-				[1,1,1,1],
-				[2,0,2,0],
-				[0,0,0,0],
-				[1,1,1,1]]}
-	];
+};
+Layouts.prototype.generate_grid = function(layout){
+	var layout_info = this.layouts[layout] || layout;
 	
-	this.generate_grid = function(layout){
-		var layout_info = layouts[layout] || layout;
+	var grid = '';
+	var width = 0;
+	var height = 0;
+	
+	for (var i in layout_info.cells){
+		var row = layout_info.cells[i];
+		var width_current = 0;
+		var offset = 0;
 		
-		var grid = '';
-		var width = 0;
-		var height = 0;
-		for (var i in layout_info.cells){
-			var row = layout_info.cells[i];
-			var width_current = 0;
-			var offset = 0;
-			
-			for (var j in row){
-				var size = row[j];
-				if (size === 0){
-					offset++;
-				} else {
-					var offset_class = '';
-					if (offset > 0){
-						offset_class = ' pre_' + offset;
-					}
-					// add a cell:
-					grid += '<div class="grid_cell grid_' + size*layout_info.size + offset_class + '"></div>\n';
+		for (var j in row){
+			var size = row[j];
+			if (size === 0){
+				offset++;
+			} else {
+				var offset_class = '';
+				if (offset > 0){
+					offset_class = ' pre_' + offset;
 				}
-				width_current += size*layout_info.size; 
+				// add a cell:
+				grid += '<div class="' + this.prefix + 'cell ' + this.prefix + size*layout_info.size + offset_class + '"></div>\n';
 			}
-			
-			// save max width:
-			if (width < width_current){
-				width = width_current;
-			}
-			height += layout_info.size;
-			
-			// close row:
-			//grid += '<div class="clear"></div>\n';
+			width_current += size*layout_info.size; 
 		}
 		
-		grid = '<div class="grid_container grid_' + height + '_' + width + '">\n' + grid + '</div>';
-		//alert(grid);
+		// save max width:
+		if (width < width_current){
+			width = width_current;
+		}
+		height += layout_info.size;
 		
-		return grid;
+		// close row:
+		//grid += '<div class="clear"></div>\n';
 	}
+	
+	grid = '<div class="grid_container ' + this.prefix + height + '_' + width + '">\n' + grid + '</div>';
+	//alert(grid);
+	
+	return grid;
 }
-
-
+Layouts.prototype.layouts = [
+	{	size: 6,
+		cells:[
+			[1,1],
+			[1,1]]},
+	{	size: 4,
+		cells:[
+			[2,0,1],
+			[0,0,1],
+			[1,1,1]]},
+	{	size: 4,
+		cells:[
+			[1,1,1],
+			[1,1,1],
+			[1,1,1]]},
+	{	size: 4,
+		cells:[
+			[1,1,1,1],
+			[2,0,2,0],
+			[0,0,0,0]]},
+	{	size: 3,
+		cells:[
+			[2,0,2,0],
+			[0,0,0,0],
+			[2,0,1,1],
+			[0,0,1,1]]},
+	{	size: 3,
+		cells:[
+			[1,1,1,1],
+			[2,0,2,0],
+			[0,0,0,0],
+			[1,1,1,1]]}
+];
