3888cfe533c6450f1c284f3bc1d21d0a28e8d911
Ilya Fadeev <ilya@Ilya-Fadeevs-MacBook-Pro.local>
2010-10-18 23:50:58 -0400

    Convert layout's grid container to dynamic element.

--- convert_dynamic.js
+++ convert_dynamic.js
@@ -16,7 +16,7 @@ var ConvertLayout = function(id){
 		function(){
 			debug('ConvertLayout: ' + this.id );
 			
-			convert_position(this.id, id);
+			create_dynamic(this.id);
 			
 			
 			//if (drag_elements.length > 1) return 0;
@@ -29,27 +29,45 @@ var ConvertLayout = function(id){
 			drag_elements.push(e1);
 		}
 	);
+	
+	// add stopping wrapper:
+	var grid_clone_id = clone_div_jq( jQ('#' + id + ' .grid_container'), id + '_grid_container' );
+	convert_position(grid_clone_id); //, id + '_grid_container'
+	
+	var e_wr = new ElementObj(grid_clone_id);
 
 	for( var i = 0; i < drag_elements.length; i++){
 		for (var j = (i+1); j < drag_elements.length; j++){
 			var R1 = new Relation('R'+i+j, drag_elements[i], drag_elements[j], RelationFuncStop);
 			debug('- R'+i+j + ': ' + drag_elements[i].id + ', ' + drag_elements[j].id);
 		}
+			
+		var R_w = new Relation('R_w'+i, e_wr, drag_elements[i], RelationFuncStopInside);
+		debug('- R_w'+i + ': ' + e_wr.id + ', ' + drag_elements[i].id);
 	}
 	
-	jQ("[id$='_clone']").remove();
+	jQ("[id$='_clone_tmp']").remove();
 	
 	debug('', 'close');
 }
 
+var create_dynamic = function(id){
+	
+	// the present div is not absolutely positioned,
+	// thus not to break current layout we create a clone of it:
+	clone_div(id, '_clone_tmp');
+	
+	convert_position(id);
 
-var convert_position = function(id, parent_id){
-	debug('[convert_position]: id = ' + id + ', parent = ' + parent_id, 'open' );
+}
+
+var convert_position = function(id, source_id){
+	var source_id = source_id || id;
+
+	debug('[convert_position]: id = ' + id, 'open' );
 	
 	debug('- getting offset:');
-	var offset = get_offset(id);
-	
-	clone_div(id, parent_id);
+	var offset = get_offset(source_id);
 	
 	debug('- setting offset to: ' + offset);
 	set_offset(id, offset);
@@ -62,19 +80,32 @@ var convert_position = function(id, parent_id){
 	debug('', 'close');
 }
 
-var clone_div = function(id, parent_id){
+var clone_div = function(id, suffix){
+	var suffix = suffix || '_clone';
+
 	debug('[clone_div]: className = ' + document.getElementById(id).className );
 	var div_coor = get_div_coor(id);
-	var clone_div = '<div id="'+id+'_clone" style="' +
+	var clone_html = '<div id="'+id+suffix+'" style="' +
 			'width: ' + div_coor.width + 'px;' +
-			'height: ' + div_coor.width + 'px;" ' +
+			'height: ' + div_coor.height + 'px;" ' +
 		'class="' + document.getElementById(id).className + ' clone" ' +
 		'z-index=0' +
 	'"></div>';
 	
-	jQ('#' + id).before(clone_div);
+	jQ('#' + id).after(clone_html);
+}
+
+var clone_div_jq = function(div_jq, id, suffix){
+	var suffix = suffix || '_clone';
+
+	var clone_div = '<div id="'+id+suffix+'" style="' +
+			'width: ' + div_jq.width() + 'px;' +
+			'height: ' + div_jq.height() + 'px;" ' +
+	'"></div>';
 	
-	clone_elements.push(id+'_clone');
+	div_jq.before(clone_div);
+	
+	return id+suffix;
 }
 
 
